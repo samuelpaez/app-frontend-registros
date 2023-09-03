34e1da1f68c7ba843ca8b8855848f89f57da5fe1
samuelpaez <73002405+samuelpaez@users.noreply.github.com>
2023-09-03 19:07:37 -0400

    registros-backend

--- src/contexto/contexto.jsx
+++ src/contexto/contexto.jsx
@@ -3,8 +3,8 @@ import { createContext, useContext, useEffect, useState } from "react";
 export const ContextoGlobal = createContext();
 
 function ProveedorDatos(props) {
-  let url2 = "https://app-registros-backend.onrender.com/";
-  let url = "http://localhost:3000/";
+  let url = "https://app-registros-backend.onrender.com/";
+  let url2 = "http://localhost:3000/";
   const [lista, setlista] = useState([]);
   const [monto, setmonto] = useState(0);
   const [tasa, settasa] = useState(0);
